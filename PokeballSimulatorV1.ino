57d310962081ccc34bd0e874a4cbc047ccac690c
Chetan Sharma <chetansharma7870@gmail.com>
2026-08-26 17:40:54 +0530

    Completed the pokeball sim V1 + Added README file + CIRCUIT DIG

--- PokeballSimulatorV1.ino
+++ PokeballSimulatorV1.ino
@@ -7,7 +7,8 @@ enum State{
   SHAKE2,
   SHAKE3,
   CAUGHT,
-  ESCAPED
+  ESCAPED,
+  RESULT
 };
 
 struct Color {
@@ -26,22 +27,23 @@ Color purple;
 Color white;
 
 
-struct Pokemons{
+struct Pokemon{
   
   int spawnThreshold;
   int catchRate;
   int pokemonID;   //pokemonID is 0 to 4
+  int rarity; // 0 - common, 1- uncommon , 2 - rare, 3- mythical
 };
 
-Pokemons bidoof;
-Pokemons pikachu;
-Pokemons beldum;
-Pokemons garchomp;
-Pokemons mewtwo;
-Pokemons currentPokemon;
+Pokemon bidoof;
+Pokemon pikachu;
+Pokemon beldum;
+Pokemon garchomp;
+Pokemon mewtwo;
+Pokemon currentPokemon;
 
 
-const int redLed = 11;
+const int redLed = 11;  // physical pins
 const int greenLed = 6;
 const int blueLed = 9;
 
@@ -103,28 +105,33 @@ void setup(){
   bidoof.catchRate = 45;
   bidoof.spawnThreshold = 75;
   bidoof.pokemonID = 0;
+  bidoof.rarity = 0;
 
-  pikachu.catchRate = 20;
+  pikachu.catchRate = 45;
   pikachu.spawnThreshold = 85;
   pikachu.pokemonID = 1;
+  pikachu.rarity = 1;
 
-  beldum.catchRate = 10;
+  beldum.catchRate = 45;
   beldum.spawnThreshold = 92;
   beldum.pokemonID = 2;
+  beldum.rarity = 1;
 
-  garchomp.catchRate = 8;
+  garchomp.catchRate = 45;
   garchomp.spawnThreshold = 97;
   garchomp.pokemonID = 3;
+  garchomp.rarity = 2;
 
-  mewtwo.catchRate = 3;
+  mewtwo.catchRate = 45;
   mewtwo.spawnThreshold = 100;
   mewtwo.pokemonID = 4;
+  mewtwo.rarity = 3;
 }
 
 
 
 
-int spawnRoll;
+int spawnRoll;  //Random value generator variables
 int catchRoll;
 
 
@@ -132,7 +139,8 @@ int catchRoll;
 
 bool previousButton = false;
 bool currentButton = false;
-enum State pokeballState = IDLE;
+
+enum State pokeballState = IDLE; //default state
 bool ledOn = false;
 
 
@@ -161,6 +169,8 @@ void ledControl(bool state){
     digitalWrite(redLed, LOW);
   }else{
     digitalWrite(redLed, HIGH);
+    digitalWrite(greenLed, HIGH);
+    digitalWrite(blueLed, HIGH);
   }
 }
 
@@ -168,6 +178,8 @@ void ledControl(bool state){
 
 
 void fadeInAnimation(int time, Color colour){
+
+
 
   for(int brightness = 0; brightness <= 255 ; brightness++){
     
@@ -209,11 +221,12 @@ void loop(){
   
   currentButton = !digitalRead(4);
 
- 
+  
 
   switch (pokeballState){
 
     case IDLE:
+      
       
       startingSequence(READY, false);
       Serial.println("IDLE");
@@ -227,7 +240,7 @@ void loop(){
 
     case THROWING:
 
-     fadeInAnimation(20, red ); // (delay, Colour )
+     fadeInAnimation(8, red ); // (delay, Colour )
      pokeballState = SHAKE1;
      delay(1000);
       Serial.println("THROWING");
@@ -247,7 +260,7 @@ void loop(){
     
     case SHAKE3:
       
-      fadeInAnimation(8, red);
+      fadeInAnimation(6, red);
       Serial.println("SHAKE 3");
       spawnRoll = random(101);
       catchRoll = random(101);
@@ -273,11 +286,11 @@ void loop(){
       Serial.println(spawnRoll);
       Serial.println("Catch");
 
-      fadeInAnimation(100, green);
+      
 
       delay(500);
 
-      pokeballState = IDLE;
+      pokeballState = RESULT;
 
       break;
 
@@ -296,12 +309,39 @@ void loop(){
 
     case RESULT:
       
-      
+      switch (currentPokemon.rarity){
+        case 0 :
 
+          fadeInAnimation(20, green);
+          Serial.println("common");
+
+          break;
+        case 1 :
+
+          fadeInAnimation(20, blue);
+          Serial.println("Un common");
+          break;
+        case 2:
+
+          fadeInAnimation(20, orange);
+          Serial.println("RARE");
+          break;
+        case 3:
+
+          fadeInAnimation(20, white);
+          Serial.println("MYTHICAL");
+          break;
+
+        
+
+      }
+
+      delay(300);
+      pokeballState = IDLE;
       
       break;
       
-
+      
       
   }
   
--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # Pokeball-Arduino-Simulator
 A pokeball (an item from an pokemon animator) catching sequence simulator using arduino 
+
+
+Description:
+   - It is a small project that includes the main function of the pokeball
+   - This project reflects the basic pokeball logic of pokemon catching
+   - It includes 5 pokemons of different spawn rates and different catch rate
+   - I loved pokemon a lot and i am an engineer too so i wanted to create a thing that concides with each other
+   - The hardest part for me is the integration of catch and spawn rate
+   
+
+
+
+FEATURES :
+    - Pokeball shake animation through blinking LEDs
+    - Different pokemon rarity has different led indicator at the end (RESULT)
+    - 5 pokemons can be obtained 
+    - It has catch rate for catching pokemon
+    - It also calculates spawn rate to choose which pokemon is chosen for catching
+    - It has different states for easy flow
+    - only supports RGB leds (single)
+
+
+
+HARDWARE:
+    - ARDUINO UNO R3   x1
+    - RGB leds         x1
+    - Push Button      x1
+    - Resistor 220ohm  x1
+    - Jumper Wire      x1
+
+
+CIRCUIT:
+
+    ![alt text](image.png)
+
+
+USUAGE:
+    - To use first press button while its in IDLE state
+    - After that "The led will turn on" going into READY TO THROW STATE
+    - then another press will intiate the catching sequence
+    - Then after catching sequence based on the catch and spawn rate you will have a RESULT
+
+FUTURE IMPROVEMENTS:
+    - Much better Animations
+    - Refactoring code with better techniques
+    - Add a buzzer for more immersive experience
+    - Add a OLED display for visual interaction
+    - Add a movable platform to simulate pokeball shake
+    - Create a 3d printed shell for proper pokeball
